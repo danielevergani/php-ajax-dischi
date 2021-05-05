be9bcf3d9d2e6ab4cf4342fa170e967603f14eed
danielevergani <dvergani94@gmail.com>
2021-05-05 15:35:33 +0200

    incluso file database.php in index

--- index-vue.php
+++ index-vue.php
@@ -1,9 +1,3 @@
-<?php
-
-include __DIR__ . "/partials/database.php"
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -21,24 +15,23 @@ include __DIR__ . "/partials/database.php"
     </header>
     <main>
     <main>
-        <!-- selezione genere
+        <!-- selezione genere -->
         <div class="genre__selection">
             <label for="genere">Scegli il tuo genere preferito</label>
             <select v-model="genreSelected" name="genere" id="genere">
                 <option  v-for="genere in genre" :value="genere"> {{genere}} </option>
             </select>
-        </div> -->
+        </div>
         
         <!--end selezione genere -->
 
         <div class="album__section">
-            <!-- <div v-for="album in albums" v-show="album.genre == genreSelected || genreSelected == 'All'" class="single_album">
+            <div v-for="album in albums" v-show="album.genre == genreSelected || genreSelected == 'All'" class="single_album">
                 <img :src="album.poster" alt="">
                 <h3 class="title">{{album.title}}</h3>
                 <p class="author">{{album.author}}</p>
                 <p>{{album.year}}</p>
-            </div> -->
-
+            </div>
         </div>
     </main>
     </main>
