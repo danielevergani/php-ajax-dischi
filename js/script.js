7af29a360fc24b4817d8ad373068ac8be3a357ce
danielevergani <dvergani94@gmail.com>
2021-05-05 16:41:37 +0200

    chiamata axios ad api

--- js/script.js
+++ js/script.js
@@ -2,5 +2,11 @@ var app = new Vue ({
     el: `#app`,
     data: {
         albums: []
+    },
+    mounted: function(){
+        axios.get("http://localhost/php-ajax-dischi/partials/api.php")
+        .then( response => {
+            console.log(response.data);
+        })
     }
 });
--- partials/api.php
+++ partials/api.php
@@ -5,6 +5,7 @@ include __DIR__ . "/database.php";
 header('Content-Type: application/json');
 
 echo json_encode($dischi);
+
 ?>
 
 <!-- http://localhost/php-ajax-dischi/partials/api.php -->
