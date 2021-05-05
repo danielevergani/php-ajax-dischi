b4f043957f0d9468ab79f308f8c6372c42e22975
danielevergani <dvergani94@gmail.com>
2021-05-05 16:46:10 +0200

    index-vue rinominato

---
+++ index2.php
@@ -0,0 +1,43 @@
+<?php
+
+include __DIR__ . "/partials/database.php";
+include __DIR__ . "/partials/header.php";
+
+
+?>
+
+    <main>
+        <!-- selezione genere
+        <div class="genre__selection">
+            <label for="genere">Scegli il tuo genere preferito</label>
+            <select v-model="genreSelected" name="genere" id="genere">
+                <option  v-for="genere in genre" :value="genere"> {{genere}} </option>
+            </select>
+        </div> -->
+        
+        <!--end selezione genere -->
+
+        <div class="album__section">
+            <!-- <div v-for="album in albums" v-show="album.genre == genreSelected || genreSelected == 'All'" class="single_album">
+                <img :src="album.poster" alt="">
+                <h3 class="title">{{album.title}}</h3>
+                <p class="author">{{album.author}}</p>
+                <p>{{album.year}}</p>
+            </div> -->
+
+            <?php foreach($dischi as $disco) :?>
+                <div class="single_album">
+                    <img src="<?php echo $disco["poster"]; ?>" alt="">
+                    <h3 class="title"><?php echo $disco["title"]; ?></h3>
+                    <p class="author"><?php echo $disco["author"]; ?></p>
+                    <p><?php echo $disco["year"]; ?></p>
+                </div>
+
+            <?php endforeach; ?>
+
+        </div>
+    </main>
+    
+        
+    </body>
+</html>
--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ var app = new Vue ({
     mounted: function(){
         axios.get("http://localhost/php-ajax-dischi/partials/api.php")
         .then( response => {
-            console.log(response.data);
+            (response.data);
         })
     }
 });
